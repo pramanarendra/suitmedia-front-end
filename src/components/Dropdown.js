36d3208f50db637807e9592cb23c392b64147141
pramanarendra <albertushandypramana@gmail.com>
2023-01-19 14:31:20 +0700

    adding component: navigation bar

---
+++ src/components/Dropdown.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import '../components/Dropdown.css'
+
+const Dropdown = () => {
+
+  const dropdownItems = ['History', 'Vision Mission']
+
+  return (
+    <>
+      <ul className="dropdown">
+        { dropdownItems.map((item, index) => {
+          return (
+            <li key={index} className='dropdown-item'>{item}</li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default Dropdown
